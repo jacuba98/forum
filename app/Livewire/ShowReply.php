78b2fff769c1c73c943530a9040ef509d5eca264
jacuba98 <jacuba98456@mail.com>
2023-11-19 14:17:59 -0500

    Editar una respuesta

--- app/Livewire/ShowReply.php
+++ app/Livewire/ShowReply.php
@@ -10,9 +10,33 @@ class ShowReply extends Component
     public Reply $reply;
     public $body = '';
     public $is_creating = false;
+    public $is_editing = false;
 
     protected $listeners = ['refresh' => '$refresh'];
 
+    public function updatedIsCreating()
+    {
+        $this->is_editing = false;
+        $this->body = '';
+    }
+
+    public function updatedIsEditing()
+    {
+        $this->is_creating = false;
+        $this->body = $this->reply->body;
+    }
+
+    public function updateReply()
+    {
+        // validate
+        $this->validate(['body' => 'required']);
+
+        // update
+        $this->reply->update(['body' => $this->body]);
+
+        // refresh
+        $this->is_editing = false;
+    }
 
     public function postChild()
     {
