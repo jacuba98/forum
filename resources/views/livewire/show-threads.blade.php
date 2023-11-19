c5e11e0bfd94760573ec26e993584d264a216c5a
jacuba98 <jacuba98456@mail.com>
2023-11-18 21:38:06 -0500

    Implementacion de diseño Hilo

--- resources/views/livewire/show-threads.blade.php
+++ resources/views/livewire/show-threads.blade.php
@@ -28,21 +28,33 @@
         @foreach ($threads as $thread)
             <div class="rounded-md bg-gradient-to-r from-slate-800 to-slate-900 hover:to-slate-800 mb-4">
                 <div class="p-4 flex gap-4">
-                    <div>Image, Avatar</div>
+                    <div>
+                        <img src="{{ $thread->user->avatar() }}" alt="{{ $thread->user->name }}" class="rounded-md">
+                    </div>
                     <div class="w-full">
                         <h2 class="mb-4 flex items-start justify-between">
                             <a href="" class="text-xl font-semibold text-white/90">
                                 {{ $thread->title }}
                             </a>
                             <span class="rounded-full text-xs py-2 px-4 capitalize"
-                                style="color: #00aced; border: 1px solid #00aced">categoria</span>
+                                style="color: {{ $thread->category->color }}; border: 1px solid {{ $thread->category->color }}">{{ $thread->category->name }}</span>
                         </h2>
                         <p class="flex items-center justify-between w-full text-xs">
                             <span class="text-blue-600 font-semibold">
-                                Usuario
+                                {{ $thread->user->name }}
                                 <span class="text-white/90">{{ $thread->created_at->diffForHumans() }}</span>
                             </span>
-                            <span class="text-slate-700">Resouestas y boton</span>
+                            <span class="flex items-center gap-1 text-slate-700">
+                                <svg aria-hidden="true" class="h-4" fill="currentColor" viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path clip-rule="evenodd"
+                                        d="M4.804 21.644A6.707 6.707 0 006 21.75a6.721 6.721 0 003.583-1.029c.774.182 1.584.279 2.417.279 5.322 0 9.75-3.97 9.75-9 0-5.03-4.428-9-9.75-9s-9.75 3.97-9.75 9c0 2.409 1.025 4.587 2.674 6.192.232.226.277.428.254.543a3.73 3.73 0 01-.814 1.686.75.75 0 00.44 1.223zM8.25 10.875a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25zM10.875 12a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zm4.875-1.125a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25z"
+                                        fill-rule="evenodd"></path>
+                                </svg>
+                                {{ $thread->replies_count }}
+                                Respuesta{{ $thread->replies_count !== 1 ? 's' : '' }} | <a href=""
+                                    class="hover:text-white">Editar</a>
+                            </span>
                         </p>
                     </div>
 
