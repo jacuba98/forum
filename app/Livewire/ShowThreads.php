9ec2542f47b5df5eadaf2c9ba29b27c6ccd160ce
jacuba98 <jacuba98456@mail.com>
2023-11-18 20:18:42 -0500

    Configuracion de categorias y preguntas

--- app/Livewire/ShowThreads.php
+++ app/Livewire/ShowThreads.php
@@ -3,11 +3,15 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Category;
 
 class ShowThreads extends Component
 {
     public function render()
     {
-        return view('livewire.show-threads');
+        $categories = Category::get();
+        return view('livewire.show-threads', [
+            'categories' => $categories
+        ]);
     }
 }
