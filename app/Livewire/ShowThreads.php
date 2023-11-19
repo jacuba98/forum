985a49d021c174027915210169e83f2b32c9075c
jacuba98 <jacuba98456@mail.com>
2023-11-18 22:44:52 -0500

    Filtro por categoria

--- app/Livewire/ShowThreads.php
+++ app/Livewire/ShowThreads.php
@@ -9,6 +9,12 @@ use App\Models\Thread;
 class ShowThreads extends Component
 {
     public $search = '';
+    public $category = '';
+
+    public function filterByCategory($category)
+    {
+        $this->category = $category;
+    }
 
     public function render()
     {
@@ -16,6 +22,11 @@ class ShowThreads extends Component
 
         $threads = Thread::query();
         $threads->where('title', 'like', "%$this->search%");
+
+        if ($this->category) {
+            $threads->where('category_id', $this->category);
+        }
+
         $threads->withCount('replies');
         $threads->latest();
         return view('livewire.show-threads', [
